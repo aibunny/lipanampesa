8af93ff1e0008aa8c2988f11ad00d2053dc9895b
aibunny <waihenyafredrick@gmail.com>
2023-06-15 19:21:01 +0300

    API

--- wallet/views.py
+++ wallet/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from . serializers import ViaLipaNaMpesa
+from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
 from . models import LipaNaMpesa
- 
+from . serializers import ViaLipaNaMpesa
 class LNMView(CreateAPIView):
     serializer_class = ViaLipaNaMpesa
-    
+    permission_classes= [AllowAny]
     def create(self,request):
         print(request.data,"this is request.data")
